81cac0fe91fa2eedd716c8785901e4371b189bd2
duchoangfe <duchoangfe@gmail.com>
2023-07-18 17:01:31 +0700

    BE: Crud category, product

--- backend-booking/src/app.js
+++ backend-booking/src/app.js
@@ -1,1 +1,19 @@
+import express from 'express';
+import authRouter from './routes/auth';
+import productRouter from './routes/product';
+import categoryRouter from './routes/category';
+import cors from 'cors';
+
+import mongoose from 'mongoose';
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', productRouter);
+app.use('/api', categoryRouter);
+app.use('/api', authRouter);
+
+mongoose.connect('mongodb://127.0.0.1:27017/we17302');
+
 export const viteNodeApp = app;
